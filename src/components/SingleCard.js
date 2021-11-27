fb239cadb5aabd8d8b4a992642238187e8a32219
MaryamSharifii <maryam.1999.853@gmail.com>
2021-11-27 19:29:24 +0330

    cards choice

--- src/components/SingleCard.js
+++ src/components/SingleCard.js
@@ -1,12 +1,20 @@
 import React from 'react'
 import "./SingleCard.css"
 
-export default function SingleCard({ card }) {
+export default function SingleCard({ card, handleChoice }) {
+
+    const handleClick = () => {
+        handleChoice(card)
+    }
     return (
         <div className="card">
             <div>
                 <img className="front" src={card.src} alt="card's front"/>
-                <img className="back" src="/img/Law of the Sea.jpg" alt="card's back" />
+                <img 
+                className="back" 
+                src="/img/Law of the Sea.jpg" 
+                onClick={handleClick} 
+                alt="card's back" />
             </div>
         </div>
     )
